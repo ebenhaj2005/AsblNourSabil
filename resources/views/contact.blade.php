dc14eed966ebe45474da302d7b4e255a03b3351a
ebenhaj2005 <145969715+ebenhaj2005@users.noreply.github.com>
2024-12-05 22:23:08 +0100

    update

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,14 +5,45 @@
 <link rel="stylesheet" href="css/contact.css">
 @endsection
 @section('content')
-<div class="team-container">
 
+<div class="Person">
+        <img class="personimg" src="images/hammadi2.jpg" alt="person" class="Person__img" width="500px" height = "500px">
+        <div class="Person__info">
+            <h1 class="Person__name">Hammadi Benhaj</h1>
+            <p class="Person__title">Président de l'Asbl Nour Sabil</p>
+            <a href="mailto:delta63012@gmail.com">delta63012@gmail.com</a>
 
-
-
+</div>
+        
 
 
 </div>
+
+<div class="Person">
+        <img class="personimg" src="images/hammadi2.jpg" alt="person" class="Person__img" width="500px" height = "500px">
+        <div class="Person__info">
+            <h1 class="Person__name">Omar Agzennai</h1>
+            <p class="Person__title">Président de l'Asbl Nour Sabil</p>
+            <a href="mailto:delta63012@gmail.com">delta63012@gmail.com</a>
+
+</div>
+        
+
+
+</div>
+<div class="Person">
+        <img class="personimg" src="images/hammadi2.jpg" alt="person" class="Person__img" width="500px" height = "500px">
+        <div class="Person__info">
+            <h1 class="Person__name">Omar Agzennai</h1>
+            <p class="Person__title">Président de l'Asbl Nour Sabil</p>
+            <a href="mailto:delta63012@gmail.com">delta63012@gmail.com</a>
+
+</div>
+        
+
+
+</div>
+
 
         @include('components.newsletter')
 
